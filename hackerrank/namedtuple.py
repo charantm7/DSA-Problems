db1d1829acd7519be10b7a674aecdd32f7e13140
charantm <charantm8787@gmail.com>
2025-11-10 22:48:16 +0530

    .

--- hackerrank/namedtuple.py
+++ hackerrank/namedtuple.py
@@ -3,3 +3,4 @@ size, fields = int(input()), input().split()
 Student = namedtuple('Student', fields)
 students = [Student(*input().split()) for _ in range(size)]
 print(sum(int(s.MARKS) for s in students)/size)
+# 
